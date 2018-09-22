a1551d1647239c8af08e6d741ee37ff7a86c96f3
Uyen Truong <haiuyentruong@gmail.com>
2018-09-22 17:06:57 +0300

    Refactor into a function

--- utbanner.c
+++ utbanner.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
+void a(void);
+
 int main(void)
+{
+    a();
+    return 0;
+}
+
+void a(void)
 {
     printf("  AAA   \n"
            " AAAAA  \n"
            "AA   AA \n"
            "AAAAAAA \n"
            "AA   AA \n");
-
-    return 0;
 }
