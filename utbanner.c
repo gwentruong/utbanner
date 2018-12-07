abbd00c9f7a115593a713ca43e1e832e6d3e8b7e
Uyen Truong <37957397+gwentruong@users.noreply.github.com>
2018-12-07 21:55:09 +0200

    Add new fonts, new function to check font (#5)
    * Add font selection function, adjust different fonts row length to be printed
    
    * Improve fonts extension
    
    * Add support for command line arguments
    
    * Add options to choose the type of command, convert non-standard arguments into default style
    
    * Edit README

--- utbanner.c
+++ utbanner.c
@@ -1,38 +1,81 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define ROWS 5
-#define SIZE 16
+#define ROWS     22
+#define SIZE     64
+#define FONT_NUM 2
 
 char ascii_art[16][ROWS][SIZE];
+char font_list[FONT_NUM][16]    = {"letters", "alpha"};
+int  font_row_length[FONT_NUM]  = {5, 22};
 
+void help_menu(int n);
+int  get_font_index(char *font_style);
+int  is_alpha(char c);
 void split_string(char array[ROWS][SIZE], char *s);
-void read_font(char letter, char letter_art[ROWS][SIZE]);
-int alpha(char c);
+void read_font(char letter, char *font_style, char letter_art[ROWS][SIZE]);
 
 int main(int argc, char **argv)
 {
-    if (argc == 1)
+    if (argc <= 1)
     {
-        printf("Usage: utbanner WORD\n");
+        help_menu(0); // Invalid command, return help menu
         return 1;
     }
 
-    for (int k = 1; k < argc; k++)
+    char *option = argv[1];
+    char *font_style;
+    int   font_index = 0;
+    int   rows;
+    int   word_index;
+
+    if (strcmp(option, "--help") == 0)
+    {
+        help_menu(0); // Show help menu
+        return 0;
+    }
+    else if (strcmp(option, "--version") == 0)
+    {
+        help_menu(1); // Show version
+        return 0;
+    }
+    else if (strcmp(option, "-f") == 0 || strcmp(option, "-i") == 0)
+    {
+        if (argc < 4) // Invalid command, return help menu
+        {
+            help_menu(0);
+            return 1;
+        }
+
+        if (strcmp(option, "-f") == 0)
+        {
+            char *font = argv[2];
+            font_index = get_font_index(font);
+        }
+        else // option == "-i"
+            font_index = atoi(argv[2]); // If error, return index 0
+
+        word_index  = 3;
+    }
+    else // Default style
+        word_index = 1;
+
+    rows        = font_row_length[font_index];
+    font_style  = font_list[font_index];
+
+    for (int k = word_index; k < argc; k++)
     {
         char *str = argv[k];
         int   len = strlen(str);
 
         for (int i = 0; i < len; i++)
         {
-            if (alpha(str[i]))
-            {
-                // Return array of substrings of current letter
-                read_font(str[i], ascii_art[i]);
-            }
+            if (is_alpha(str[i]))
+                read_font(str[i], font_style, ascii_art[i]);
         }
         // Print out the whole line ascii_art[i][j] in 3D array
-        for (int j = 0; j < ROWS; j++)
+        for (int j = 0; j < rows; j++)
         {
             for (int i = 0; i < len; i++)
                 printf("%s", ascii_art[i][j]);
@@ -40,6 +83,52 @@ int main(int argc, char **argv)
         }
     }
     return 0;
+}
+
+// Help menu
+void help_menu(int n)
+{
+    if (n == 0) // Print help menu
+    {
+        printf("Usage: utbanner [options] [font] [word]\n"
+               "Options:\n"
+               "-i\tChoose font by font index\n"
+               "-f\tChoose font by font name\n"
+               "--help\n\tPrint help menu\n"
+               "--version\n\tPrint the lastest version\n"
+               "\n"
+               "  index\tfont\n"
+               "* 0\tletters (Default)\n"
+               "* 1\talpha\n");
+    }
+    else // Print latest version
+    {
+        printf("utbanner v0.1\n"
+               "Copyright (C) 2018 Uyen Truong\n"
+               "Contact me if found bugs via haiuyentruong(at)gmail(dot)com\n");
+    }
+
+}
+
+// Checking for font style
+int get_font_index(char *font_style)
+{
+    for (int i = 0; i < FONT_NUM; i++)
+    {
+        if (strcmp(font_style, font_list[i]) == 0)
+            return i;
+    }
+
+    return 0;
+}
+
+// Check if the letter is is_alphaical
+int is_alpha(char c)
+{
+    if ((c > 64 && c < 91) || (c > 96 && c < 123))
+        return 1;
+    else
+        return 0;
 }
 
 // Split string into substrings and fill into array
@@ -70,14 +159,14 @@ void split_string(char array[ROWS][SIZE], char *s)
 }
 
 // Read each character from font files list
-void read_font(char letter, char letter_art[ROWS][SIZE])
+void read_font(char letter, char *font_style, char letter_art[ROWS][SIZE])
 {
     FILE *fp;
     char  path[32];
-    char  buf[128];
+    char  buf[1024];
     int   size;
 
-    sprintf(path, "./fonts/letters/%c", letter);
+    sprintf(path, "./fonts/%s/%c", font_style, letter);
 
     fp = fopen(path, "r");
 
@@ -90,13 +179,4 @@ void read_font(char letter, char letter_art[ROWS][SIZE])
     split_string(letter_art, buf);
 
     fclose(fp);
-}
-
-// Check if the letter is alphabetical
-int alpha(char c)
-{
-    if ((c > 64 && c < 91) || (c > 96 && c < 123))
-        return 1;
-    else
-        return 0;
 }
